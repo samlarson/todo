1b4745bf75122b5d5978bdad1819a23f17e4d23c
samlarson <slarson207@gmail.com>
2021-04-18 20:34:20 -0400

    Added functionality for moving entries between lists (assign)

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ use chrono::prelude::*;
 // use serde_json::Result;
 // use std::error::Error;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Item {
     id: i32,
     descr: String,
@@ -100,19 +100,36 @@ fn get_item_fields() -> [String; 5] {
     array
 }
 
+fn get_list_path(list_name: &str) -> String {
+    let mut file_path = String::new();
+    match list_name {
+        "todo" => {
+            file_path = "/home/eidolon/git/todo/src/todo.json".to_owned();
+        },
+        "active" => {
+            file_path = "/home/eidolon/git/todo/src/active.json".to_owned();
+        },
+        "done" => {
+            file_path = "/home/eidolon/git/todo/src/done.json".to_owned();
+        },
+        _ => unreachable!(),
+    }
+    file_path
+}
+
 fn read_list(list_name: &str) -> Result<Vec<Item>, Error> {
     let mut file_path = String::new();
     match list_name {
         "todo" => {
-            println!("Reading contents of Todo List...");
+            // println!("Reading contents of Todo List...");
             file_path = "/home/eidolon/git/todo/src/todo.json".to_owned();
         },
         "active" => {
-            println!("Reading contents of Active List...");
+            // println!("Reading contents of Active List...");
             file_path = "/home/eidolon/git/todo/src/active.json".to_owned();
         },
         "done" => {
-            println!("Reading contents of Done List...");
+            // println!("Reading contents of Done List...");
             file_path = "/home/eidolon/git/todo/src/done.json".to_owned();
         },
         _ => unreachable!(),
@@ -127,7 +144,7 @@ fn read_list(list_name: &str) -> Result<Vec<Item>, Error> {
 }
 
 fn display_list(list: Vec<Item>) {
-    println!("Displaying contents of list...");
+    // println!("Displaying contents of list...");
     // for elem in list.iter() {
     //     println!("{:?}", elem);
     // }
@@ -163,6 +180,18 @@ fn max_list_id(list: &Vec<Item>) -> i32 {
         }
     }
     max_id
+}
+
+fn validate_entry_id(list: &Vec<Item>, item_id: i32) -> bool {
+    for elem in list.iter() {
+        if elem.id == item_id {
+            // let entry: Item = elem.cloned();
+            // let mut entry = Item::from(elem.clone());
+            // entry
+            return true
+        }
+    }
+    false
 }
 
 fn get_curr_date() -> String {
@@ -214,10 +243,43 @@ fn write_entry(mut todo_list: Vec<Item>, field_array: [String; 5]) -> Result<(),
 
         todo_list.push(entry_struct);
         let serialized = serde_json::to_string_pretty(&todo_list).unwrap();
-
         write("/home/eidolon/git/todo/src/todo.json", serialized).expect("Unable to write file");
         Ok(())
     }
+}
+
+fn move_entry(src_name: &str, dst_name: &str, mut src_list: Vec<Item>, mut dst_list: Vec<Item>, item_id: i32) -> Result<(), Error> {
+    // let item: Item = validate_entry_id(&src_list, item_id);
+    // match validate_entry_id(&src_list, item_id) {
+    //     true => println!(""),
+    //     false => panic!("Specified Item ID was not found in list."),
+    // }
+    if validate_entry_id(&src_list, item_id) {
+        let index_opt: Option<usize> = src_list.iter().position(|x|x.id == item_id);
+        let index: usize = index_opt.unwrap();
+
+
+        let item: Item = src_list[index].to_owned();
+        // let item: Item = src_list.iter().position(|&x| x.id == item_id);
+
+        // Pop
+        // src_list.remove(src_list.iter().position(|x| *x.id == item_id).expect("Entry not found"));
+        src_list.retain(|x|x.id != item_id);
+        let src_serial = serde_json::to_string_pretty(&src_list).unwrap();
+        let src_path = get_list_path(src_name);
+        write(src_path, src_serial).expect("Unable to write source file");
+
+        // Push
+        dst_list.push(item);
+        let dst_serial = serde_json::to_string_pretty(&dst_list).unwrap();
+        let dst_path = get_list_path(dst_name);
+        write(dst_path, dst_serial).expect("Unable to write destination file");
+
+        Ok(())
+    } else {
+        panic!("Specified Item ID was not found in list.");
+    }
+
 }
 
 fn main() {
@@ -235,7 +297,7 @@ fn main() {
     }
 
     if m.is_present("add") {
-        println!("Detected *add* subcommand");
+        // println!("Detected *add* subcommand");
         let todo_list: Vec<Item> = read_list("todo").unwrap();
         let field_array = get_item_fields();
         write_entry(todo_list, field_array);
@@ -243,11 +305,40 @@ fn main() {
 
     match m.subcommand() {
         Some(("list", list_matches)) => {
-            println!("Detected *list* subcommand");
-            println!("{}", list_matches.value_of("listname").unwrap());
+            // println!("Detected *list* subcommand");
+            // println!("{}", list_matches.value_of("listname").unwrap());
             let read_list_name = list_matches.value_of("listname").unwrap();
             let list: Vec<Item> = read_list(read_list_name).unwrap();
             display_list(list);
+        }
+        Some(("assign", assign_matches)) => {
+            println!("Detected *assign* subcommand");
+            match assign_matches.subcommand() {
+                Some(("todo", assign_todo_matches)) => {
+                    println!("{}", assign_todo_matches.value_of("itemid").unwrap());
+                }
+                Some(("done", assign_done_matches)) => {
+                    println!("{}", assign_done_matches.value_of("itemid").unwrap());
+                }
+                Some(("active", assign_active_matches)) => {
+                    // println!("{}", assign_active_matches.value_of("itemid").unwrap());
+                    match assign_active_matches.subcommand() {
+                        Some(("item", assign_active_item_matches)) => {
+                            // println!("{}", assign_active_item_matches.value_of("itemid").unwrap());
+                            let str_id = assign_active_item_matches.value_of("itemid").unwrap();
+                            let item_id = str_id.parse::<i32>().unwrap();
+                            let todo_list: Vec<Item> = read_list("todo").unwrap();
+                            let active_list: Vec<Item> = read_list("active").unwrap();
+                            move_entry("todo", "active", todo_list, active_list, item_id);
+                        }
+                        Some(("rand", _)) => {
+                            println!("Assign random entry to active");
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                _ => unreachable!(),
+            }
         }
         _ => {},
     }
@@ -257,3 +348,13 @@ fn main() {
 // TODO: clean up write_entry()
 // TODO: handle categorical variables in struct?
 // TODO: move on to the next subcommand, start to thing about handling next level down
+
+// TODO: check if a given list exists
+// TODO: move add subcommand to match structure
+// TODO: finish list functionality (todo features, active and done lists)
+// TODO: remove commented out code
+
+// TODO: add second parameter to move_entry, you need src and dst lists to keep fxn abstract
+// TODO: figure out most efficient way to pop struct off vector
+// TODO: change subcommand reference to get_list_path, pass read_list by getting that path
+// TODO: dynamic filepaths for actual executable folder
